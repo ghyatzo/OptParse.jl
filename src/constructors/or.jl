26776c0760f22ff607a58bf5bbc0cc1ec544a93b
C. Schenone <dev@cschen.cc>
2025-11-22 19:00:20 +0100

    (refactor): move the or constructor to use generated functions to help with type instability.

--- src/constructors/or.jl
+++ src/constructors/or.jl
@@ -13,6 +13,7 @@ ConstrOr(parsers::Tuple) = let
     inner_state = map(parsers) do p
         none(ParseSuccess{tstate(p)})
     end
+    actual_parsers = map(unwrapunion, parsers)
     ConstrOr{
         Union{map(tval, parsers)...},
         OrState{typeof(inner_state)},
@@ -21,58 +22,118 @@ ConstrOr(parsers::Tuple) = let
     }((0, inner_state), parsers)
 end
 
-function parse(p::ConstrOr{T, OrState{S}}, ctx::Context{OrState{S}})::ParseResult{OrState{S},String} where {T, S}
-    error = length(ctx.buffer) < 1 ?
+
+@generated function _generated_or_parse(parsers::PTup, ctx::Context) where {PTup <: Tuple}
+    preamble = quote
+        error = length(ctx.buffer) < 1 ?
             (0, "No matching option or command.") : (0, "Unexpected option or subcommand: $(ctx.buffer[1])")
+    end
+    N = length(PTup.parameters)
+    unrolled_loop = Expr(:block)
+    for i in 1:N
+        parser_t = PTup.parameters[i]
+        push!(unrolled_loop.args, quote
+            parser = parsers[$i]::$parser_t
+            innerstate = ctx.state[2][$i]
+            childstate = is_error(innerstate) ? parser.initialState : unwrap(innerstate).next.state
+            childctx = @set ctx.state = childstate
+
+            result = (@unionsplit parse(parser, childctx))::ParseResult{tstate(parser), String}
+            if !is_error(result) && length(unwrap(result).consumed) > 0 # (ignores constants)
+                parse_ok = unwrap(result)
+
+                # If we successfully match something, but the current state is telling us that we've already matched
+                # something else,
+                # and those two things aren't the same thing, then error. 'Or' only matches one parser.
+                if ctx.state[1] != 0 && ctx.state[1] != $i
+                    already_matched_state_id = ctx.state[1]
+                    return ParseErr(length(ctx.buffer) - length(parse_ok.next.buffer),
+                        "$(unwrap(ctx.state[2][already_matched_state_id]).consumed[1]) and $(parse_ok.consumed[1]) can't be used together.")
+                end
+
+                new_innerstate = set(ctx.state[2], IndexLens($i), some(parse_ok))
 
 
-    # @info Base.remove_linenums!(_generated_or_parse(p.parsers, ctx))
-    for (i, parser) in enumerate(p.parsers)
-
-        innerstate = ctx.state[2][i]
-        childstate = is_error(innerstate) ?  parser.initialState : unwrap(innerstate).next.state
-        childctx = @set ctx.state = childstate
-
-        result = parse(parser, childctx)::ParseResult{tstate(parser), String}
-        if !is_error(result) && length(unwrap(result).consumed) > 0 # (ignores constants)
-            parse_ok = unwrap(result)
-
-            # If we successfully match something, but the current state is telling us that we've already matched
-            # something else,
-            # and those two things aren't the same thing, then error. 'Or' only matches one parser.
-            if ctx.state[1] != 0 && ctx.state[1] != i
-                already_matched_state_id = ctx.state[1]
-                return ParseErr(length(ctx.buffer) - length(parse_ok.next.buffer),
-                    "$(unwrap(ctx.state[2][already_matched_state_id]).consumed[1]) and $(parse_ok.consumed[1]) can't be used together.")
-            end
-
-            new_innerstate = set(ctx.state[2], IndexLens(i), some(parse_ok))
-
-
-            return ParseOk(
-                parse_ok.consumed, Context(
-                    parse_ok.next.buffer,
-                    (i, new_innerstate),
-                    parse_ok.next.optionsTerminated
+                return ParseOk(
+                    parse_ok.consumed, Context(
+                        parse_ok.next.buffer,
+                        ($i, new_innerstate),
+                        parse_ok.next.optionsTerminated
+                    )
                 )
-            )
-        elseif is_error(result)
-            if error[1] < unwrap_error(result).consumed
-                parse_err = unwrap_error(result)
-                error = (parse_err.consumed, parse_err.error)
+            elseif is_error(result)
+                if error[1] < unwrap_error(result).consumed
+                    parse_err = unwrap_error(result)
+                    error = (parse_err.consumed, parse_err.error)
+                end
             end
-        end
-        # i += 1
+        end)
     end
 
-    return ParseErr(error[1], error[2])
+    epilogue = :(return ParseErr(error[1], error[2]))
+
+    return quote
+        $preamble
+        $unrolled_loop
+        $epilogue
+    end
 end
+
+(parse(p::ConstrOr{T, OrState{S}}, ctx::Context{OrState{S}})::ParseResult{OrState{S},String}) where {T, S} =
+    _generated_or_parse(p.parsers, ctx)
+
+# function _parse(p::ConstrOr{T, OrState{S}}, ctx::Context{OrState{S}})::ParseResult{OrState{S},String} where {T, S}
+#     error = length(ctx.buffer) < 1 ?
+#             (0, "No matching option or command.") : (0, "Unexpected option or subcommand: $(ctx.buffer[1])")
+
+#     @info Base.remove_linenums!(_generated_or_parse(p.parsers, ctx))
+#     i = 1
+#     for parser in p.parsers
+
+#         innerstate = ctx.state[2][i]
+#         childstate = is_error(innerstate) ?  parser.initialState : unwrap(innerstate).next.state
+#         childctx = @set ctx.state = childstate
+
+#         result = (@unionsplit parse(parser, childctx))::ParseResult{tstate(parser), String}
+#         if !is_error(result) && length(unwrap(result).consumed) > 0 # (ignores constants)
+#             parse_ok = unwrap(result)
+
+#             # If we successfully match something, but the current state is telling us that we've already matched
+#             # something else,
+#             # and those two things aren't the same thing, then error. 'Or' only matches one parser.
+#             if ctx.state[1] != 0 && ctx.state[1] != i
+#                 already_matched_state_id = ctx.state[1]
+#                 return ParseErr(length(ctx.buffer) - length(parse_ok.next.buffer),
+#                     "$(unwrap(ctx.state[2][already_matched_state_id]).consumed[1]) and $(parse_ok.consumed[1]) can't be used together.")
+#             end
+
+#             new_innerstate = set(ctx.state[2], IndexLens(i), some(parse_ok))
+
+
+#             return ParseOk(
+#                 parse_ok.consumed, Context(
+#                     parse_ok.next.buffer,
+#                     (i, new_innerstate),
+#                     parse_ok.next.optionsTerminated
+#                 )
+#             )
+#         elseif is_error(result)
+#             if error[1] < unwrap_error(result).consumed
+#                 parse_err = unwrap_error(result)
+#                 error = (parse_err.consumed, parse_err.error)
+#             end
+#         end
+#         i += 1
+#     end
+
+#     return ParseErr(error[1], error[2])
+# end
 
 function complete(p::ConstrOr{T}, orstate::OrState{S})::Result{T,String} where {T,S}
     orstate[1] == 0 && return Err("No matching option or command.")
     ith, allmaybestates = orstate
 
-    result = complete(p.parsers[ith], unwrap(allmaybestates[ith]).next.state)
+    result = @unionsplit complete(p.parsers[ith], unwrap(allmaybestates[ith]).next.state)
 
     if !is_error(result)
         return Ok(unwrap(result))
